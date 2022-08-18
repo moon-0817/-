9486fbcac17a8cacd4ecc286cf6c00cde67631e1
月亮打烊了 <2657593811@qq.com>
2022-08-18 23:34:30 +0800

    day05-8-18

---
+++ day05/pm/demo/src/views/fuzi.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>父子组件</h1>
+    <p>{{ n }}</p>
+
+    <zi :zhi="n"></zi>
+    <zi></zi>
+  </div>
+</template>
+
+<script>
+import zi from "@/components/fuzi/zi.vue";
+export default {
+  data() {
+    return {
+      n: 123456,
+    };
+  },
+  components: {
+    zi,
+  },
+  methods: {
+    ff() {
+      console.log("啊哈哈");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
