783da85468265376bbcf9cebd12e4fbc10bcb3ce
月亮打烊了 <2657593811@qq.com>
2022-08-20 22:46:19 +0800

    day06

---
+++ day06/demo/src/views/houdai.vue
@@ -0,0 +1,25 @@
+
+<template>
+<div>
+    <h1>后代 provide inject</h1>
+    <p>flag值为:{{flag}}</p>
+</div>
+</template>
+
+<script>
+export default {
+    inject:['flag'],
+  name: '',
+  components: {},
+  data () {
+    return {
+    }
+  },
+  created () { },
+  mounted () { },
+  methods: {}
+}
+</script>
+<style scoped lang='scss'>
+
+</style>
---
+++ day06/vuex/src/App.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div id="app">
+    <h1>
+      state: {{ $store.state.n }} getters {{ $store.getters.mony }} mapState
+      {{ n }}
+
+      <button @click="$store.commit('jiajia', 1)">+1</button>
+      <button @click="fn1">+1</button>
+      <button @click="jiajia(1)">+1</button>
+      <button @click="jj(3)">+1</button>
+      <button @click="jjj(3)">+1</button>
+    </h1>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapActions, mapState } from "vuex";
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    fn1() {
+      this.$store.commit("jiajia", 1);
+      // console.log(111);
+    },
+    ...mapMutations(["jiajia"]),
+    ...mapMutations({
+      jj: "jiajia",
+    }),
+    ...mapMutations({
+      jjj: (commit, n) => commit("jiajia", n),
+    }),
+  },
+  computed: {
+    cn() {
+      return this.$store.state.mony;
+    },
+    ...mapState(["n"]),
+  },
+};
+</script>
+<style scoped lang='scss'>
+</style>
