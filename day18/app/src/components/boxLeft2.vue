753cd6b2eff5299d0337997f453a00b6e672cc01
月亮打烊了 <2657593811@qq.com>
2022-09-06 21:28:28 +0800

    day18

---
+++ day18/app/src/components/boxLeft2.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div class="boxLeft2"></div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style scoped lang='scss'>
+.boxLeft2 {
+  width: 300px;
+  height: 410px;
+}
+</style>
---
+++ day18/app/src/components/boxRight1.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div class="boxRight1">
+    产品销售渠道分析
+    <div class="top">
+      <dv-capsule-chart :config="config" style="width: 300px; height: 150px" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      config: {
+        data: [
+          {
+            name: "南阳",
+            value: 167,
+          },
+          {
+            name: "周口",
+            value: 67,
+          },
+          {
+            name: "漯河",
+            value: 123,
+          },
+          {
+            name: "郑州",
+            value: 55,
+          },
+          {
+            name: "西峡",
+            value: 98,
+          },
+        ],
+      },
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style scoped lang='scss'>
+.boxRight1 {
+  width: 300px;
+  height: 410px;
+}
+</style>
